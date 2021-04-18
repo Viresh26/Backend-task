fb748ac9fa9751664f64dc199b6924be73d483b8
VireshKashetti <kashettivir@gmail.com>
2021-04-18 15:40:41 +0530

    Final working code

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
 const express = require("express");
+const { off } = require("./db");
 const app = express();
 const pool = require("./db");
 
-app.get("/branches/", async (req, res) => {
+app.get("/api/branches/autocomplete", async (req, res) => {
 
     try {
         // res.json(req.query)
 
-        const {q} = req.query;
-        const {limit} = req.query;
-        const {offset} = req.query;
-        
-        const branch = await pool.query("SELECT * FROM branches WHERE 'branch' ILIKE $1 Order By 'ifsc' LIMIT $2 OFFSET$3 ",[`%${q}%`],[`${limit}`],[`${offset}`]);
+        let {q} = req.query;
+        let {limit} = req.query;
+        let {offset} = req.query;
 
-        // console.log(branch);
+        let sql = `SELECT * FROM branches WHERE branch ILIKE $1 ORDER BY ifsc LIMIT $2 OFFSET $3`;
+        let values = [`%${q}%`, `${limit}`, `${offset}`];
+
+        // THIS IS WHERE I AM MESSING UP PROBABLY
+        
+        let branch = await pool.query(sql,values);
+        // console.log({q}, {limit}, {offset});
         // const branch = await pool.query("SELECT * FROM branches WHERE");
 
+        res.json(branch.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+app.get("/api/branches", async (req, res) => {
+
+    try {
+        // res.json(req.query)
+
+        let {q} = req.query;
+        let {limit} = req.query;
+        let {offset} = req.query;
+
+        let sql = `SELECT * FROM branches WHERE 
+        ifsc ILIKE $1 OR
+        branch ILIKE $1 OR
+        address ILIKE $1 OR
+        city ILIKE $1 OR
+        district ILIKE $1 OR
+        state ILIKE $1
+        ORDER BY ifsc
+        LIMIT $2 OFFSET $3`;
+
+        let values = [`%${q}%`, `${limit}`, `${offset}`];
+
+        let branch = await pool.query(sql,values);
+        
         res.json(branch.rows);
     } catch (err) {
         console.error(err.message);
